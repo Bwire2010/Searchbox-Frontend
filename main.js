88826d9ff9766b79b2911c33d002b9b870fe7981
patrobas <patrobasbwire@gmail.com>
2024-01-21 16:51:53 +0300

    added readme

--- main.js
+++ main.js
@@ -1,54 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const searchInput = document.getElementById('searchInput');
-//     const searchButton = document.getElementById('searchButton');
-//     const searchResultsDropdown = document.getElementById('searchResultsDropdown');
-//     const loadingSpinner = document.getElementById('loadingSpinner');
-
-//     console.log('searchInput:', searchInput);
-//     console.log('searchButton:', searchButton);
-//     console.log('searchResultsDropdown:', searchResultsDropdown);
-
-//     searchButton.addEventListener('click', handleSearch);
-
-//     function handleSearch() {
-//         const searchTerm = searchInput.value.trim();
-
-//         if (searchTerm.length === 0) {
-//             // Clear the dropdown if the search term is empty
-//             searchResultsDropdown.innerHTML = '';
-//             return;
-//         }
-
-//         // Display loading spinner
-//         loadingSpinner.style.display = 'block';
-
-//         // Make a request to the backend API
-//         fetch(`http://127.0.0.1:3000/searches?query=${encodeURIComponent(searchTerm)}`)
-//             .then(response => response.json())
-//             .then(data => {
-//                 console.log('Received data:', data);
-//                 // Update the dropdown with the search results
-//                 searchResultsDropdown.innerHTML = '';  // Clear the dropdown
-
-//                 // Append all saved searches (existing + new) to the dropdown
-//                 data.forEach(result => {
-//                     const resultItem = document.createElement('div');
-//                     resultItem.textContent = result;
-//                     searchResultsDropdown.appendChild(resultItem);
-//                 });
-
-//                 // Hide loading spinner after displaying results
-//                 loadingSpinner.style.display = 'none';
-//             })
-//             .catch(error => {
-//                 console.error('Error fetching data:', error);
-
-//                 // Hide loading spinner on error
-//                 loadingSpinner.style.display = 'none';
-//             });
-//     }
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.getElementById('searchInput');
     const searchButton = document.getElementById('searchButton');
@@ -74,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
         loadingSpinner.style.display = 'block';
 
         // Make a request to the backend API
-        fetch(`https://searchbox-005p.onrender.com/searches?query=${encodeURIComponent(searchTerm)}`)
+        fetch(`http://127.0.0.1:3000/searches?query=${encodeURIComponent(searchTerm)}`)
             .then(response => response.json())
             .then(data => {
                 console.log('Received data:', data);
